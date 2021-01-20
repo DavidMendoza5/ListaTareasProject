47d83046f5339666f066fac974feab9672f30f10
David <cruz-azul99@hotmail.com>
2021-01-20 11:21:24 -0600

    New method and variable added - Variable numTareas was created, this variable is sent by props to NuevaTarea component and also is used by the method decrementarContador - Method decrementarContador was created, this method is sent as prop to lista-tareas

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
     <div class="jumbotron">
       <!-- Uso de un componente al que se le envían props y uso de la directiva v-bind para crear un enlace simple -->
       <Titulo :titulo="titulo_prop"></Titulo>
+      Tareas: {{ numTareas }}
       <!-- El mismo nombre que se usa en los props es el mismo que se debe declarar en los props del componente -->
-      <NuevaTarea :tareas="tareas"></NuevaTarea>
+      <!-- El $event sirve para obtener el valor que retorna el evento que solicitamos, en este caso es un 1 -->
+      <NuevaTarea :tareas="tareas" v-on:incrementarContador="numTareas+=$event"></NuevaTarea>
       <!-- Esto es lo mismo que poner tal cual el nombre del componente, de hecho Vue lo agrega por ti si lo pones de esta forma -->
-      <lista-tareas :tareas="tareas"></lista-tareas>
+      <lista-tareas :tareas="tareas" :decrementarContador="decrementarContador" ></lista-tareas>
     </div>
   </div>
 </template>
@@ -26,6 +28,7 @@ export default {
   data() {
     return {
       titulo_prop: 'Lista de tareas',
+      numTareas: 3,
       tareas: [
         {
           texto: 'Aprender VueJs',
@@ -40,6 +43,11 @@ export default {
           terminada: false
         }
       ]
+    }
+  },
+  methods: {
+    decrementarContador() {
+      return this.numTareas--
     }
   }
 }
