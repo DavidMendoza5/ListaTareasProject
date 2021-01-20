c1533fac584fc645e8109406cf59f1821b389056
David <cruz-azul99@hotmail.com>
2021-01-20 11:26:01 -0600

    Validation added to titulo prop

--- src/components/Titulo.vue
+++ src/components/Titulo.vue
@@ -6,6 +6,12 @@
 export default {
     // Los props debe llevar el mismo nombre que el atributo que se envía desde el componente padre, al igual que al hacer la interpolación
     // debe tener el mismo nombre
-    props: ['titulo']
+    // Se usan objetos cuando se quieren hacer validaciones, en este caso le decimos a Vue que si no recibe ninguna prop, ponga por defecto Tareas
+    props: {
+        titulo: {
+            type: String,
+            default: 'Tareas'
+        }
+    }
 }
 </script>
