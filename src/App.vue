f8217d5f6e636a9e1025539063a6e71def9722f8
David <cruz-azul99@hotmail.com>
2021-01-20 12:49:03 -0600

    Variable numTareas is no longer sent to the components

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <div id="app" class="container">
     <div class="jumbotron">
       <!-- Uso de un componente al que se le envían props y uso de la directiva v-bind para crear un enlace simple -->
-      <Titulo :titulo="titulo_prop" :numTareas="numTareas"></Titulo>
+      <!-- <Titulo :titulo="titulo_prop" :numTareas="numTareas"></Titulo> -->
+      <Titulo :titulo="titulo_prop"></Titulo>
       <!-- El mismo nombre que se usa en los props es el mismo que se debe declarar en los props del componente -->
       <!-- El $event sirve para obtener el valor que retorna el evento que solicitamos, en este caso es un 1 -->
-      <NuevaTarea :tareas="tareas" v-on:incrementarContador="numTareas+=$event"></NuevaTarea>
+      <!-- <NuevaTarea :tareas="tareas" v-on:incrementarContador="numTareas+=$event"></NuevaTarea> -->
+      <NuevaTarea :tareas="tareas"></NuevaTarea>
       <!-- Esto es lo mismo que poner tal cual el nombre del componente, de hecho Vue lo agrega por ti si lo pones de esta forma -->
-      <lista-tareas :tareas="tareas" :decrementarContador="decrementarContador" ></lista-tareas>
+      <!-- <lista-tareas :tareas="tareas" :decrementarContador="decrementarContador" ></lista-tareas> -->
+      <lista-tareas :tareas="tareas"></lista-tareas>
     </div>
   </div>
 </template>
