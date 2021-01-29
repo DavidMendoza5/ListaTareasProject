0925fb4228e207a2742839b7839a65c24cae4e7d
David <cruz-azul99@hotmail.com>
2021-01-29 10:41:22 -0600

    New method created and a request was added - Method estado was created to change the status of a task - A http request was added, this request is a delete, so we delete a task on firebase

--- src/components/ListaTareas.vue
+++ src/components/ListaTareas.vue
@@ -1,15 +1,15 @@
 <template>
     <ul class="list-group">
         <!-- v-bind:class lo que hace es que si tarea.terminada es verdadero, entonces le aplica la clase de css -->
-        <li v-for="tarea in tareas" :key="tarea.texto" class="list-group-item" v-bind:class="{terminada: tarea.terminada}">
+        <li v-for="(tarea, indice) in tareas" :key="tarea.texto" class="list-group-item" v-bind:class="{terminada: tarea.terminada}">
             {{ tarea.texto }}
             <span class="pull-right">
                 <button type="button" class="btn btn-success btn-xs glyphicon glyphicon-ok" 
-                        @click="tarea.terminada = !tarea.terminada">Terminar</button>
+                        @click="estado(indice)">Terminar</button>
             </span>
             <span class="pull-right">
                 <button type="button" class="btn btn-danger btn-xs glyphicon glyphicon-ok" 
-                    @click="eliminarTarea(tarea)">Borrar</button>
+                    @click="eliminarTarea(indice)">Borrar</button>
             </span>
         </li>
     </ul>
@@ -21,11 +21,20 @@ import { bus } from '../main.js'
 export default {
     props: ['tareas', 'decrementarContador'],
     methods: {
-        eliminarTarea(tarea) {
-            let indice = this.tareas.indexOf(tarea)
+        estado(indice) {
+            let terminada = this.tareas[indice].terminada = !this.tareas[indice].terminada
+            let id = this.tareas[indice].id
+
+            this.$http.patch('tareas/' + id + '.json', {
+                terminada: terminada
+            }).then(respuesta => console.log(respuesta))
+        },
+        eliminarTarea(indice) {
+            let id = this.tareas[indice].id
             this.tareas.splice(indice,1)
             // this.decrementarContador()
             bus.$emit('actualizarContador', this.tareas.length)
+            this.$http.delete('tareas/' + id + '.json').then(respuesta => console.log(respuesta))
         }
     }
 }
