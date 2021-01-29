34c3f62f2642e703acbe66cf9b2e84f04e5dfb93
David <cruz-azul99@hotmail.com>
2021-01-29 10:43:11 -0600

    A url was added as a root url for the http requests

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 
+Vue.http.options.root = 'https://ejemplovue-485c4-default-rtdb.firebaseio.com'
+
 Vue.config.productionTip = false
 
 // Creamos una variable que nos va a servir como bus de eventos
